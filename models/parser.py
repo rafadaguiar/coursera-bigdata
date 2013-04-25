823d1e900b61b09af0a676f906194022ef924437
Rafael Aguiar <rafael@criticallab.in>
2013-04-25 18:48:02 -0300

    util

--- models/parser.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-def parse_entry(s):
-    m = re.search("(?P<dir>(\w+/)*\w+):::(?P<authors>.+):::(?P<title>.+).", s)
-    d = m.groupdict()
-    d['authors'] = d['authors'].split('::')
-    return d
